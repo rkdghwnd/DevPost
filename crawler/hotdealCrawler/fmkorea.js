784855c79bced413010fe7f8c1156bb237c16f10
rkdghwnd <ghwnd6448@gmail.com>
2023-03-31 16:22:47 +0900

    cralwer update

--- crawler/hotdealCrawler/fmkorea.js
+++ crawler/hotdealCrawler/fmkorea.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const fmkorea = async () => {
   try {
+    const revisionInfo = await browserFetcher.download("756035");
     const browser = await puppeteer.launch({
       headless: process.env.STATUS === "production",
       args: ["--window-size=1920,1080"],
